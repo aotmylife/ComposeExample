0c26f33fea3e18ae12bf9e65daf76950e3f98919
todsavorn <todsavorn@azurecomputer.com>
2025-07-14 13:56:17 +0700

    remove code not use

--- app/src/main/java/com/example/composeexample/MainActivity.kt
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -9,21 +9,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
-import com.example.composeexample.color.Color.Blue
-import com.example.composeexample.color.Color.Chartreuse
-import com.example.composeexample.color.Color.LightBlue
-import com.example.composeexample.color.Color.Navy
 import com.example.composeexample.ui.MainScreen
 import com.example.composeexample.ui.MainViewModel
 import com.example.composeexample.ui.theme.ComposeExampleTheme
@@ -42,21 +27,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-private val DarkColorScheme = darkColorScheme(
-
-    surface = Blue,
-    onSurface = Navy,
-    primary = Navy,
-    onPrimary = Chartreuse
-)
-
-private val LightColorScheme = lightColorScheme(
-    surface = Blue,
-    onSurface = Color.White,
-    primary = LightBlue,
-    onPrimary = Navy
-)
 
 @Preview(showBackground = true)
 @Composable
